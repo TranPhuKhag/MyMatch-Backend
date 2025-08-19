5beb259d1644108cd9b7c7350768bddff2e83726
TranPhuKhag <trankhang0990@gmail.com>
2025-08-19 17:26:58 +0700

    [MM-29] restore @PreAuthorize annotations in UniversityService

--- backend/src/main/java/com/mymatch/service/UniversityService.java
+++ backend/src/main/java/com/mymatch/service/UniversityService.java
@@ -7,12 +7,12 @@ import com.mymatch.dto.response.university.UniversityResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface UniversityService {
-//    @PreAuthorize("hasAuthority('university:create')")
+    @PreAuthorize("hasAuthority('university:create')")
     UniversityResponse createUniversity(UniversityCreationRequest req);
     UniversityResponse getById(Long id);
-//    @PreAuthorize("hasAuthority('university:update')")
+    @PreAuthorize("hasAuthority('university:update')")
     UniversityResponse updateUniversity(Long id, UniversityUpdateRequest req);
-//    @PreAuthorize("hasAuthority('university:delete')")
+    @PreAuthorize("hasAuthority('university:delete')")
     UniversityResponse deleteUniversity(Long id);
     PageResponse<UniversityResponse> getAllUniversities(int page, int size, String sort);
 }
