a550e6578bdec0fe174a928e3bba3591a99978bd
TranPhuKhag <trankhang0990@gmail.com>
2025-09-05 18:47:19 +0700

    [MM-57] API Get Chat Message

--- backend/src/main/java/com/mymatch/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/mymatch/repository/ChatMessageRepository.java
@@ -4,5 +4,8 @@ import com.mymatch.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findAllByConversationIdOrderByCreateAtAsc(Long conversationId);
 }
