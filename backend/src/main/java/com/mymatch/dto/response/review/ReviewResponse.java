ce7a4da176c38ec426ca6f23c0296ce84af0b554
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-10-08 11:54:23 +0700

    Add lecturer to review respose

--- backend/src/main/java/com/mymatch/dto/response/review/ReviewResponse.java
+++ backend/src/main/java/com/mymatch/dto/response/review/ReviewResponse.java
@@ -1,6 +1,7 @@
 package com.mymatch.dto.response.review;
 
 import com.mymatch.dto.response.course.CourseResponse;
+import com.mymatch.dto.response.lecturer.LecturerResponse;
 import com.mymatch.dto.response.reviewdetail.ReviewDetailResponse;
 import com.mymatch.dto.response.semester.SemesterResponse;
 import com.mymatch.dto.response.student.StudentResponse;
@@ -17,6 +18,7 @@ import java.util.List;
 public class ReviewResponse {
     Long id;
     StudentResponse student;
+    LecturerResponse lecturer;
     CourseResponse course;
     SemesterResponse semester;
     Double overallScore;
